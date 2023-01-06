5dcba2d959e85be1b85ebb9debaa2ce3881c49fd
darideveloper <darideveloper@gmail.com>
2023-01-05 21:32:58 -0600

    add scraping manager and loop csv rows

--- __main__.py
+++ __main__.py
@@ -1,9 +1,22 @@
 import os
-from config import Config
-from logs import logger
+import csv
 from scraping_manager.automate import Web_scraping
 
 def main (): 
+    # csv path
+    csv_path = os.path.join(os.path.dirname(__file__), "pages.csv")
+    if not os.path.isfile(csv_path):
+        print ("File 'pages.csv' not found")
+        return ""
+    
+    # Read csv file content
+    with open (csv_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter = ",")
+        csv_rows = set(list(csv_reader))
+        
+    # Loop through csv rows
+    for (page, ) in csv_rows:
+        print (page)
 
 
 if __name__ == "__main__":
--- logs.py
+++
@@ -1,9 +0,0 @@
-import os
-import logging
-
-# logs to file
-logging.basicConfig(filename='app.log', format='%(asctime)s - %(filename)s (%(lineno)s) - %(levelname)s - %(message)s')
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
