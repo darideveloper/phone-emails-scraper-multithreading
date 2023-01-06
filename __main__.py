dde277fb6d00ed7b152a927355a88e7b6b9b7179
darideveloper <darideveloper@gmail.com>
2023-01-05 21:49:35 -0600

    get emails and phones with bs4

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 import os
 import csv
+import requests
+from bs4 import BeautifulSoup
 from scraping_manager.automate import Web_scraping
 
 def main (): 
@@ -11,14 +13,33 @@ def main ():
     
     # Read csv file content
     with open (csv_path, "r") as file:
-        pages = file.readlines()
+        pages = set(file.readlines())
         
     # Loop through csv rows
     for page in pages:
         
+        # Format page
         page = page.strip()
+        if not page.startswith("http"):
+            page = "https://" + page
+            
+        # variables for storage results
+        found_emails = []
+        found_phones = []
+            
+        # Scrape page with requests and parse to soup
+        res = requests.get(page)
+        if res.status_code != 200:
+            print ("Error scraping page: " + page)
+            continue
+        print ("Scraping page: " + page)
+        soup = BeautifulSoup(res.text, "html.parser")
         
-        print (page)
+        # Get phone and email with css selectors
+        found_emails += list(map(lambda email: email.get_text(), soup.select('[href^="mailto:"]')))
+        found_phones += list(map(lambda phone: phone.get_text(), soup.select('[href^="tel:"]')))
+        print ()
+        
 
 
 if __name__ == "__main__":
